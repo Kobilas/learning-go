8b6a83a58a641271e8a96bb7a433666638451405
Matthew Kobilas <mattkobi789@gmail.com>
2020-06-23 11:33:40 -0400

    Working with waitgroups and buffered channels

---
+++ covid_grocer/grocer.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func wait(i int, wg *sync.WaitGroup, out chan string) {
+	fmt.Println("Customer", i, "has entered the queue")
+	shop(i, wg, out)
+}
+
+func shop(i int, wg *sync.WaitGroup, out chan string) {
+	var timeToShop int = (rand.Intn(6) + 5) * 100
+	fmt.Println("Customer", i, "has entered the grocery store")
+	time.Sleep(time.Duration(timeToShop) * time.Millisecond)
+	out <- fmt.Sprintf("Customer %d has left the grocery store", i)
+	wg.Done()
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	var numCustomers int = rand.Intn(51) + 50
+	var wg sync.WaitGroup
+	var timeToCustomer, size, arrived int
+	output := make(chan string, 5)
+	fmt.Println("You think", numCustomers, "people are going to come today")
+	for arrived < numCustomers-1 {
+		if size < 5 {
+			timeToCustomer = (rand.Intn(6) + 5) * 100
+			time.Sleep(time.Duration(timeToCustomer) * time.Millisecond)
+			wg.Add(1)
+			go wait(arrived, &wg, output)
+			arrived++
+			size++
+		} else {
+			fmt.Println(<-output)
+			size--
+		}
+	}
+	wg.Wait()
+
+}
